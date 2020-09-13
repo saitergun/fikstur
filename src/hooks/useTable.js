954eea339dcced6ceb54d5e45eda3d76cfa1625f
saitergun <tiasweb@gmail.com>
2020-09-13 23:16:42 +0300

    fix table sorting

--- src/hooks/useTable.js
+++ src/hooks/useTable.js
@@ -43,33 +43,33 @@ const useTable = (season) => {
       return played;
     }
 
-    function getTeamCountsHeadToHead(firstTeamId, againstTeamId) {
-      let countFirstTeamPoints = 0;
-      let countAgainstTeamPoints = 0;
-      let countFirstTeamGoals = 0;
-      let countAgainstTeamGoals = 0;
+    // function getTeamCountsHeadToHead(firstTeamId, againstTeamId) {
+    //   let countFirstTeamPoints = 0;
+    //   let countAgainstTeamPoints = 0;
+    //   let countFirstTeamGoals = 0;
+    //   let countAgainstTeamGoals = 0;
 
-      const homeMatch = matches.find((match) => match.home === firstTeamId && match.away === againstTeamId);
-      const awayMatch = matches.find((match) => match.home === againstTeamId && match.away === firstTeamId);
+    //   const homeMatch = matches.find((match) => match.home === firstTeamId && match.away === againstTeamId);
+    //   const awayMatch = matches.find((match) => match.home === againstTeamId && match.away === firstTeamId);
 
-      if (homeMatch?.score && awayMatch?.score) {
-        countFirstTeamPoints += homeMatch.score.home === homeMatch.score.away ? 1 : homeMatch.score.home > homeMatch.score.away ? 3 : 0;
-        countAgainstTeamPoints += homeMatch.score.away === homeMatch.score.home ? 1 : homeMatch.score.away > homeMatch.score.home ? 3 : 0;
+    //   if (homeMatch?.score && awayMatch?.score) {
+    //     countFirstTeamPoints += homeMatch.score.home === homeMatch.score.away ? 1 : homeMatch.score.home > homeMatch.score.away ? 3 : 0;
+    //     countAgainstTeamPoints += homeMatch.score.away === homeMatch.score.home ? 1 : homeMatch.score.away > homeMatch.score.home ? 3 : 0;
 
-        countFirstTeamPoints += awayMatch.score.home === awayMatch.score.away ? 1 : awayMatch.score.away > awayMatch.score.home ? 3 : 0;
-        countAgainstTeamPoints += awayMatch.score.away === awayMatch.score.home ? 1 : awayMatch.score.home > awayMatch.score.away ? 3 : 0;
+    //     countFirstTeamPoints += awayMatch.score.home === awayMatch.score.away ? 1 : awayMatch.score.away > awayMatch.score.home ? 3 : 0;
+    //     countAgainstTeamPoints += awayMatch.score.away === awayMatch.score.home ? 1 : awayMatch.score.home > awayMatch.score.away ? 3 : 0;
 
-        countFirstTeamGoals = homeMatch.score.home + awayMatch.score.away;
-        countAgainstTeamGoals = awayMatch.score.home + homeMatch.score.away;
-      }
+    //     countFirstTeamGoals = homeMatch.score.home + awayMatch.score.away;
+    //     countAgainstTeamGoals = awayMatch.score.home + homeMatch.score.away;
+    //   }
 
-      return {
-        countFirstTeamPoints,
-        countAgainstTeamPoints,
-        countFirstTeamGoals,
-        countAgainstTeamGoals,
-      };
-    }
+    //   return {
+    //     countFirstTeamPoints,
+    //     countAgainstTeamPoints,
+    //     countFirstTeamGoals,
+    //     countAgainstTeamGoals,
+    //   };
+    // }
 
     let table = teams.map((team) => {
       // filter and map played matches by team
@@ -105,35 +105,35 @@ const useTable = (season) => {
 
     table = table.sort((a, b) => {
       // if season not started for each teams
-      if (b.countPlayed === 0 && a.countPlayed === 0) {
-        const nameTeamA = a.nameShort.toLocaleLowerCase('tr-TR');
-        const nameTeamB = b.nameShort.toLocaleLowerCase('tr-TR');
+      // if (b.countPlayed === 0 && a.countPlayed === 0) {
+      //   const nameTeamA = a.nameShort.toLocaleLowerCase('tr-TR');
+      //   const nameTeamB = b.nameShort.toLocaleLowerCase('tr-TR');
 
-        if (nameTeamA < nameTeamB) {
-          return -1;
-        }
+      //   if (nameTeamA < nameTeamB) {
+      //     return -1;
+      //   }
 
-        if (nameTeamA > nameTeamB) {
-          return 1;
-        }
+      //   if (nameTeamA > nameTeamB) {
+      //     return 1;
+      //   }
 
-        return 0;
-      }
+      //   return 0;
+      // }
 
       // if equal points
       if (b.countPoints === a.countPoints) {
-        const counts = getTeamCountsHeadToHead(a.id, b.id);
+        // const counts = getTeamCountsHeadToHead(a.id, b.id);
 
         // if played two matches
-        if (counts) {
-          const { countFirstTeamPoints, countAgainstTeamPoints, countFirstTeamGoals, countAgainstTeamGoals } = counts;
+        // if (counts) {
+        //   const { countFirstTeamPoints, countAgainstTeamPoints, countFirstTeamGoals, countAgainstTeamGoals } = counts;
 
-          if (countFirstTeamPoints === countAgainstTeamPoints) {
-            return countAgainstTeamGoals - countFirstTeamGoals;
-          }
+        //   if (countFirstTeamPoints === countAgainstTeamPoints) {
+        //     return countAgainstTeamGoals - countFirstTeamGoals;
+        //   }
 
-          return countAgainstTeamPoints - countFirstTeamPoints;
-        }
+        //   return countAgainstTeamPoints - countFirstTeamPoints;
+        // }
 
         // if equal goals difference
         if (b.countGoalsDifference === a.countGoalsDifference) {
