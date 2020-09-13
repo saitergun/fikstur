0d41e96c24a2417a8d48bd9bfcb7b952bab783a1
saitergun <tiasweb@gmail.com>
2020-09-13 23:03:18 +0300

    sticky to fixed app navbar

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const AppNavbar = () => (
-  <nav className="sticky left-0 top-0 right-0 bg-purple-600 z-10">
-    <span className="sm:max-w-lg grid grid-cols-2 mx-auto">
+  <nav className="fixed left-0 top-0 right-0 h-12 bg-purple-600 z-10">
+    <span className="sm:max-w-lg h-12 grid grid-cols-2 mx-auto">
       <NavLink
         className="flex items-center justify-center border-b-4 border-transparent active:bg-purple-700 text-white leading-none py-3"
         activeClassName="border-purple-800 pointer-events-none"
