01e1cd5598e73e38af06352c0000dee847ba7c24
saitergun <tiasweb@gmail.com>
2020-09-16 00:44:24 +0300

    retouch useTeam

--- src/hooks/useTeam.js
+++ src/hooks/useTeam.js
@@ -4,23 +4,13 @@ import { useSelector } from 'react-redux';
 const useTeam = (id = 0) => {
   const [team, setTeam] = useState(null);
 
-  const state = useSelector((state) => state);
+  const { teams } = useSelector((state) => state.data);
 
   useEffect(() => {
-    const find = state.data.teams.find((t) => t.id === id);
+    const find = teams.find((team) => team.id === id);
 
-    if (find) {
-      setTeam({
-        id,
-        nameTff: find.nameTff,
-        name: find.name,
-        nameShort: find.nameShort,
-        logo: require(`../media/teams/logos/120x120/${id}.png`)
-      });
-    } else {
-      setTeam(null);
-    }
-  }, [id, state.data.teams]);
+    setTeam(find ?? null);
+  }, [id, teams]);
 
   return team;
 };
